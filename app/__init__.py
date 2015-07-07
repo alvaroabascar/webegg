4c511c5b0cd858269d42e20d8e779a16102d3184
alvaro <alvaroabascar@gmail.com>
2015-07-07 19:58:47 +0200

    update app name

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask, request, redirect, url_for, g
 
 # Import Blueprints
 # -------------------------------------------------------------------
-from .main.views import main_app
+from .main_app.views import main_app
+from .other_app.views import other_app
 
 # Start Flask
 # -------------------------------------------------------------------
@@ -13,3 +14,4 @@ app = Flask(__name__)
 # Register Blueprints
 # -------------------------------------------------------------------
 app.register_blueprint(main_app, url_prefix='')
+app.register_blueprint(other_app, url_prefix='/other')
