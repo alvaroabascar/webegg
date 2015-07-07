32be442c412667400eed2db1620251f5ecc9de91
alvaro <alvaroabascar@gmail.com>
2015-07-07 19:24:15 +0200

    u

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,8 @@
-from flask import Blueprint, render_template, abort
-from jinja2 import TemplateNotFound
+from flask import Blueprint, render_template
 
-main_app = Blueprint('simple_page', __name__,
-                     template_folder='templates',
-                     static_folder='static')
+main_app = Blueprint('main_app', __name__,
+                     template_folder='templates')
 
 @main_app.route('/')
 def main():
-    return render_template('index.html')
+    return "HI"
